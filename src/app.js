80ef42714134646523c9d486a0e92949bb514239
Coby Tamayo <ctamayo@sitecrafting.com>
2019-11-22 17:11:46 -0800

    init

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import prettier from 'prettier'
+
+const App = (props) => {
+  return (
+    <h1>Hello, Prettier!</h1>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
